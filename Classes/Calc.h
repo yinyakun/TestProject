5ba27e3d26187e8f5b566be0b8b32dcab458e39a
yakun yin <yinyk1@lenovo.com>
2017-06-20 10:09:11 +0800

    tijiao

---
+++ Classes/Calc.h
@@ -0,0 +1,13 @@
+//
+//  Calc.h
+//  Test11
+//
+//  Created by yakun yin on 2017/6/20.
+//  Copyright © 2017年 yakun yin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calc : NSObject
+
+@end
---
+++ Classes/Calc.m
@@ -0,0 +1,13 @@
+//
+//  Calc.m
+//  Test11
+//
+//  Created by yakun yin on 2017/6/20.
+//  Copyright © 2017年 yakun yin. All rights reserved.
+//
+
+#import "Calc.h"
+
+@implementation Calc
+
+@end
